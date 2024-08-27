7e6c9acf34b199e61b0138aad55b2b75b80f039b
Lord Daud <93244091+SonyChand@users.noreply.github.com>
2024-08-28 01:18:14 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,7 +6,7 @@ class Dashboard extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        user();
+        admin();
         date_default_timezone_set('Asia/Jakarta');
     }
     public function index()
